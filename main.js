394d094cfdbc4e057a9a41f2bd2f04d6954a152c
Iftiak Siddique Mahdi <122094318+yepboi@users.noreply.github.com>
2025-03-30 14:05:07 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+console.log('Hello World!');
+
+function func() {
+  var pass = document.getElementById('password').value;
+
+  if (pass == 'Mahdi') {
+    
+    alert('welcome ADMiN...//')
+    window.location.assign("mahdi.html")
+  }
+  else if (pass == 'Farisha') {
+    alert('Successfully loged in.!')
+    window.location.assign('farisha.html')
+    
+  }
+  else if (pass == 'Faraz') {
+  alert('Successfully loged in.!')
+  window.location.assign('faraz.html')
+    
+  }
+  else if (pass == 'Aria') {
+  alert('Successfully loged in.!')
+  window.location.assign('aria.html')
+  
+}
+else if (pass == 'Ardi') {
+  alert('Successfully loged in.!')
+  window.location.assign('ardi.html')
+  
+}
+else if (pass == 'Unisha') {
+  alert('Successfully loged in.!')
+  window.location.assign('unisha.html')
+  
+}
+else if (pass == 'Rizwan') {
+  alert('Successfully loged in.!')
+  window.location.assign('rizwan.html')
+  
+}
+else if (pass == 'Ahnaf') {
+  alert('Successfully loged in.!')
+  window.location.assign('ahnaf.html')
+  
+}
+else if (pass == 'Hamdan') {
+  alert('Successfully loged in.!')
+  window.location.assign('hamdan.html')
+  
+}
+else if (pass == 'Kaif') {
+  alert('Successfully loged in.!')
+  window.location.assign('kaif.html')
+  
+}
+else if (pass == 'Mahid') {
+  alert('Successfully loged in.!')
+  window.location.assign('mahid.html')
+  
+}
+
+else if (pass == 'admin') {
+  alert('Successfully loged in.!')
+  window.location.assign('test.html')
+  
+}  
+  
+  
+  
+  
+  
+  else {
+    alert("wrong value");
+  }
+  
+}
